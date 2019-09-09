443d0836a36c84febf041ebbb4b8a02d3dacd5f8
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-09 10:05:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |element| 
-    element.include?(cheese_types)
+    cheeses.include?(element)
   end
 end
