d45b349c5c4b346dd733f6bebe29ab0b78cd2dc1
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-09 09:40:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |element| 
-    puts "#{element}\n"
+  array.each_with_index do |element, index| 
+    puts "#{index} #{element}"
   end
 end
 
